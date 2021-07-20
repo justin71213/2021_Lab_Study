057e0d248c0d686fe4e9ea04ca3a97b00b833c5c
maglili <gibson870811@gmail.com>
2021-07-20 18:20:19 +0800

    add week0_ex2

--- Week0/WarmUp_Ex2/README.md
+++ Week0/WarmUp_Ex2/README.md
@@ -1,1 +1,40 @@
-# 這是用來放第二題(EX2):EX2.Two Sum的地方
+# EX2. Two Sum的地方
+
+## Intro
+
+[1. Two Sum](https://leetcode.com/problems/two-sum/)
+
+## Method 1: O($n^2$)
+
+**Brute-force search:** 使用雙層 for 迴圈尋找符合 target 的元素。
+
+**code:**
+
+```python
+def twoSum(self, nums: int, target: int):
+    """
+    1. assume that each input would have exactly one solution
+    2. you may not use the same element twice.
+    """
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+```
+
+## Method 2: O($n$)
+
+[Source](https://leetcode.com/problems/two-sum/discuss/17/Here-is-a-Python-solution-in-O(n)-time)
+
+**code:**
+
+```python
+def twoSum(self, nums: int, target: int):
+    goal = {}
+    for idx, num in enumerate(nums):
+        remaining = target - num
+        if num in goal:
+            return [goal[num], idx]
+        else:
+            goal[remaining] = idx
+```
--- Week0/WarmUp_Ex2/lichang_1.py
+++ Week0/WarmUp_Ex2/lichang_1.py
@@ -4,17 +4,17 @@ class Solution:
         1. assume that each input would have exactly one solution
         2. you may not use the same element twice.
         """
-        # for i in range(len(nums)):
-        #     for j in range(i + 1, len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return [i, j]
-        goal = {}
-        for idx, num in enumerate(nums):
-            remaining = target - num
-            if num in goal:
-                return [goal[num], idx]
-            else:
-                goal[remaining] = idx
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        # goal = {}
+        # for idx, num in enumerate(nums):
+        #     remaining = target - num
+        #     if num in goal:
+        #         return [goal[num], idx]
+        #     else:
+        #         goal[remaining] = idx
 
 
 if __name__ == "__main__":
